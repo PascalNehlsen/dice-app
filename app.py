8872fa2ba84156f20505123892d45d0320f2acdd
PascalNehlsen <pascaldev92@gmail.com>
2025-05-12 22:01:28 +0200

    chore: added recent throws

--- app.py
+++ app.py
@@ -8,13 +8,17 @@ OWNER = environ.get("OWNER", "Mr. X")
 BG_COLOR = environ.get("BG_COLOR", "#0c7077")
 DICE_COLOR = environ.get("DICE_COLOR", "#eb856b")
 
+recent_throws: list[int] = []
+result: int = 0
+
 @app.route("/")
 def init():
-    return render_template('index.html', OWNER=OWNER, BG_COLOR=BG_COLOR, DICE_COLOR=DICE_COLOR)
+    return render_template('index.html', OWNER=OWNER, BG_COLOR=BG_COLOR, DICE_COLOR=DICE_COLOR, recent_throws=recent_throws)
 
 @app.route("/dice")
 def dice():
     result: int = random.randint(1, 6)
+    recent_throws.append(result)
     return render_template('dice.html', result=result, OWNER=OWNER, BG_COLOR=BG_COLOR, DICE_COLOR=DICE_COLOR)
 
 @app.route("/proof")
